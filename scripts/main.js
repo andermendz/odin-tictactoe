ac701912e35bb267fe883101bd9a7754d844b2ea
andermendz <andersonmendz19@gmail.com>
2024-01-06 20:45:07 -0500

    added code for reference

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,24 @@
 // function that return  its own properties
 // object variable of that function
+function createUser (name){
+    const discordName = '@' + name;
+
+    let reputation = 0;
+
+    const getReputation = () => reputation;
+    const giveReputation = () => reputation++;
+
+    return {name, discordName, getReputation, giveReputation}
+}
+
+
+const ander = createUser('ander');
+
+console.log(ander.getReputation())
+
+ander.giveReputation();
+ander.giveReputation();
+
+console.log(ander.getReputation());
+
+console.log({name: ander.name, reputation: ander.getReputation()});
