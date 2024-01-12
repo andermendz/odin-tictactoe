a906fb16026ae15169119b1891b13a8af0f034a0
andermendz <andersonmendz19@gmail.com>
2024-01-12 18:42:55 -0500

    player turns prototype done

--- main.js
+++ main.js
@@ -3,22 +3,19 @@
 
 // factory function
 
-const playerFactory = (name,simbol, points) => {
-    const addPoints = () => points++;
-    const getPoints = () => console.log(points);
-  
-    return { simbol, addPoints, getPoints };
-  };
-  
+const playerFactory = (name, simbol, points) => {
+  const addPoints = () => points++;
+  const getPoints = () => console.log(points);
 
-  
+  return { name, simbol, addPoints, getPoints };
+};
+
 const game = (() => {
-    
   let board = ["", "", "", "", "", "", "", "", ""];
   let boardSpaces = document.querySelectorAll(".game-grid-space");
   let gameOptionsActions = document.getElementById("game-options-actions");
   let gameOptionsMessage = document.getElementById("game-options-message");
-  
+
   let startButton = document.getElementById("start-button");
 
   startButton.onclick = () => {
@@ -43,56 +40,101 @@ const game = (() => {
 </button>
 `;
 
+    let gameModeTwoPlayers = document.querySelector(
+      ".game-mode-button.twoplayers"
+    );
 
-let gameModeTwoPlayers = document.querySelector('.game-mode-button.twoplayers')
+    gameModeTwoPlayers.onclick = () => {
+      boardSpaces.forEach((space) => {
+        space.textContent = "";
+      });
+      let playerOne = playerFactory("Player 1", "X", 0);
+      let playerTwo = playerFactory("Player 2", "0", 0);
 
-gameModeTwoPlayers.onclick = (() => {
-    console.log("ddad")
-   
-
-    boardSpaces.forEach((space) => {
-        space.onclick = (() => {
-            let simbol = 'O'
-            space.textContent = simbol;
-
-            board[space.id] = simbol;
-            console.log(board);
-            boardChecker(board);
-            console.log(boardChecker(board))
-        })
-    })
+      let turn = 1;
+      let simbol = playerOne.simbol;
     
+      console.log({ playerOne, playerTwo });
 
-    // TO-DO
+      boardSpaces.forEach((space) => {
+        space.onclick = () => {
+          
 
-    // add functional 2 players mode
-    // add AI mode
-    // add rounds 
-    // add restart
-})
+          board[space.id] = simbol;
 
-const boardChecker = (board) => {
-    if ((board[0] == 'X' || board[0] == 'O') && board[0] === board[1] && board[1] === board[2]) {
-      return true;
-    } else if ((board[2] == 'X' || board[2] == 'O') && board[2] === board[5] && board[5] === board[8]) {
-      return true;
-    } else if ((board[8] == 'X' || board[8] == 'O') && board[8] === board[7] && board[7] === board[6]) {
-      return true;
-    } else if ((board[6] == 'X' || board[6] == 'O') && board[6] === board[3] && board[3] === board[0]) {
-      return true;
-    } else if ((board[0] == 'X' || board[0] == 'O') && board[0] === board[4] && board[4] === board[8]) {
-      return true;
-    } else if ((board[6] == 'X' || board[6] == 'O') && board[6] === board[4] && board[4] === board[2]) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+          boardSpaces.forEach((space)=>{
+            space.textContent = board[space.id];
+          })
+         
+          console.log(board);
+          boardChecker(board);
+          console.log(boardChecker(board));
+         
+          if(turn === 1){
+            turn = 2;
+            simbol = playerTwo.simbol;
+          } else if (turn === 2){
+            turn = 1;
+            simbol = playerOne.simbol;
+          }
+
+        };
+      });
+
+      // TO-DO
+
+      // add functional 2 players mode
+      // add AI mode
+      // add rounds
+      // add restart
+    };
+
+    const boardChecker = (board) => {
+      if (
+        (board[0] == "X" || board[0] == "O") &&
+        board[0] === board[1] &&
+        board[1] === board[2]
+      ) {
+        return true;
+      } else if (
+        (board[2] == "X" || board[2] == "O") &&
+        board[2] === board[5] &&
+        board[5] === board[8]
+      ) {
+        return true;
+      } else if (
+        (board[8] == "X" || board[8] == "O") &&
+        board[8] === board[7] &&
+        board[7] === board[6]
+      ) {
+        return true;
+      } else if (
+        (board[6] == "X" || board[6] == "O") &&
+        board[6] === board[3] &&
+        board[3] === board[0]
+      ) {
+        return true;
+      } else if (
+        (board[0] == "X" || board[0] == "O") &&
+        board[0] === board[4] &&
+        board[4] === board[8]
+      ) {
+        return true;
+      } else if (
+        (board[6] == "X" || board[6] == "O") &&
+        board[6] === board[4] &&
+        board[4] === board[2]
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    };
   };
 
   const startGame = () => {};
 
-  const boardMechanics = (() => {
+  const boardMechanics = () => {
     // for (let i = 0; i < board.length; i++){
 
     //     let choice = prompt(`pick X or O for position ${[i]} `  )
@@ -107,9 +149,5 @@ const boardChecker = (board) => {
     // }
 
     console.log(board);
-
-  
-  });
-
-
+  };
 })();
