4f865a8933ac75f413b67c6f89dda4ac22f3d28e
andermendz <andersonmendz19@gmail.com>
2024-01-17 19:47:31 -0500

    removed unnecessary code

--- main.js
+++ main.js
@@ -61,10 +61,82 @@ Restart
     gameOptionsMessage.textContent = "Select a Game Mode";
     gameOptionsActions.innerHTML = modesMenu;
 
-    let gameModeTwoPlayers = document.querySelector(
-      ".game-mode-button.twoplayers"
-    );
+    let gameModeTwoPlayers = document.querySelector(".game-mode-button.twoplayers");
+    let gmaeModeAI = document.document.querySelector(".game-mode-button.ai");
 
+    gameModeAI.onclick = () => {
+      gridSpaces.forEach((space) => {
+        space.style.opacity = 1;
+        board = [];
+        space.classList.remove("selected");
+        space.textContent = "";
+      });
+
+      let playerOne = playerFactory("Player 1", "X", 0);
+      let playerTwo = playerFactory("Player 2", "O", 0);
+
+      let turn = 1;
+      let simbol = playerOne.simbol;
+
+      gameOptionsMessage.textContent = `${playerOne.name} Turn`;
+      gameOptionsActions.innerHTML = twoPlayersRestartMenu;
+
+      let twoPlayersRestartButton = document.querySelector(
+        ".twoplayers-restart"
+      );
+      twoPlayersRestartButton.onclick = () => {
+        gameModeTwoPlayers.onclick();
+      };
+
+      console.log({ playerOne, playerTwo });
+      console.log(turn);
+
+
+      gridSpaces.forEach((space) => {
+        space.onclick = () => {
+          if (board[space.id] == ("X" || "O")) {
+            console.log("selected");
+          } else if (board[space.id] == null) {
+            console.log("not selected");
+
+            board[space.id] = simbol;
+            console.log(board[space.id]);
+            space.classList.add("selected");
+
+            gridSpaces.forEach((space) => {
+              space.textContent = board[space.id];
+            });
+
+            console.log(board);
+            boardChecker(board);
+            console.log(boardChecker(board));
+
+            if (boardChecker(board)) {
+              gameOptionsMessage.textContent = `Player ${turn} WINS`;
+              board = [];
+              gridSpaces.forEach((space) => {
+                space.classList.add("selected");
+                board[space.id] = "X";
+              });
+            } else {
+              if (turn === 1) {
+                turn = 2;
+                simbol = playerTwo.simbol;
+                gameOptionsMessage.textContent = `${playerTwo.name} Turn`;
+              } else if (turn === 2) {
+                turn = 1;
+                simbol = playerOne.simbol;
+                gameOptionsMessage.textContent = `${playerOne.name} Turn`;
+              }
+            }
+          }
+
+          console.log(turn);
+        };
+      });
+    }
+
+    // TWO PLAYERS GAME MODE 
     gameModeTwoPlayers.onclick = () => {
       gridSpaces.forEach((space) => {
         space.style.opacity = 1;
@@ -91,6 +163,8 @@ Restart
 
       console.log({ playerOne, playerTwo });
       console.log(turn);
+
+
       gridSpaces.forEach((space) => {
         space.onclick = () => {
           if (board[space.id] == ("X" || "O")) {
@@ -140,6 +214,7 @@ Restart
       // add visual help
     };
 
+    // BOARD CHECKING MECHANISM
     const boardChecker = (board) => {
       if (
         (board[0] === "X" || board[0] === "O") &&
@@ -189,22 +264,5 @@ Restart
     };
   };
 
-  const startGame = () => {};
-
-  const boardMechanics = () => {
-    // for (let i = 0; i < board.length; i++){
-
-    //     let choice = prompt(`pick X or O for position ${[i]} `  )
-    //     while (choice !== 'O' && choice !== 'X'){
-
-    //         choice = prompt(`pick X or O for position ${[i]} `  )
-    //         console.log(choice)
-    //     }
-
-    //     board[i] = choice;
-
-    // }
-
-    console.log(board);
-  };
+ 
 })();
